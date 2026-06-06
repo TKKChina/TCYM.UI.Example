4307fc8a32d5ae4bc1e8f771726cad1a2ee7760b
TCYM <1345715960@qq.com>
2026-06-06 19:59:04 +0800

    启用性能日志与优化样式注册方式
    - 添加对 `TCYM.UI.Enums` 的引用。
    - 启用帧时间日志与渲染类型分析日志。
    - 调整帧时间日志输出频率为每 60 帧。
    - 优化全局样式注册方式为 `RegisterGlobalDefaultsCss`。
    - 新增样式属性 `Cursor`，默认值为 `UICursor.Grab`。

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using TCYM.UI.Core;
 using TCYM.UI.Elements;
+using TCYM.UI.Enums;
 using TCYM.UI.Example.Page.Layout;
 using TCYM.UI.Helpers;
 
@@ -8,11 +9,14 @@ internal class Program
     public static void Main()
     {
         // 是否启用帧时间日志（输出每帧的CPU和GPU时间，单位毫秒），可用于性能分析和调优。启用后会在控制台输出每帧的渲染时间信息，帮助开发者了解UI渲染的性能瓶颈。
-        UISystem.EnableFrameTimingLog = false;
+        UISystem.EnableFrameTimingLog = true;
+        // 是否启用渲染类型分析日志（输出每帧的渲染类型分析信息）。启用后会在控制台输出每帧的渲染类型分析结果，包括不同渲染类型的占比和性能数据。这对于优化UI渲染性能非常有帮助，尤其是在复杂UI场景下，可以帮助开发者识别哪些渲染类型可能导致性能问题。
+        UISystem.EnableRenderTypeProfile = true;
         // 帧时间日志的阈值和输出频率设置（仅在启用帧时间日志时生效）。FrameTimingLogThresholdMs 设置了日志输出的时间阈值，只有当某帧的CPU或GPU渲染时间超过这个值时才会输出日志。FrameTimingLogIntervalFrames 设置了日志输出的频率，表示每隔多少帧输出一次日志。合理设置这两个参数可以帮助开发者聚焦于性能问题较严重的帧，同时避免过多的日志输出干扰分析。
         UISystem.FrameTimingLogThresholdMs = 1;
         // 设置帧时间日志的输出频率（单位：帧）。例如，设置为1表示每帧都输出日志，设置为10表示每10帧输出一次日志。合理设置这个参数可以帮助开发者在性能分析时获得足够的数据，同时避免过多的日志输出干扰分析。
-        UISystem.FrameTimingLogIntervalFrames = 1;
+        UISystem.FrameTimingLogIntervalFrames = 60;
+
         // 是否启用GPU初始化日志（输出GPU相关的初始化信息和错误日志）。启用后会在控制台输出GPU设备的相关信息、驱动版本、支持的功能等，以及在GPU初始化过程中遇到的任何错误。这对于调试和优化GPU渲染性能非常有帮助，尤其是在不同平台和设备上运行时。
         UISystem.EnableGpuInitLog = true;
         UISystem.Initialize("TCYM", 1620, 800, true, 30);
@@ -23,7 +27,7 @@ internal class Program
         TCYM.UI.Binding.Generated.GeneratedBindingAccessors_TCYM_UI_Example.InitGenerated();
 
         // === 全局通配符默认样式：字体16px，颜色 #000000 ===
-        new DefaultUIStyle().ParseFromCss("*{font-size:16px;color: rgba(0,0,0,1);}");
+        UISystem.RegisterGlobalDefaultsCss("*{font-size:16px;color: rgba(0,0,0,1);}");
         UISystem.LoadStyleFile("res://TCYM.UI.Example/Page.com.css");
         var root = manager.Root;
         root.SetStyle(new DefaultUIStyle
@@ -40,6 +44,7 @@ internal class Program
                 BoxShadowSpread = 10,
                 BoxShadowOffsetY = 2,
                 BoxShadowBlur = 4,
+                Cursor = UICursor.Grab
             },
         });
         root.AddChild(new Layout());
